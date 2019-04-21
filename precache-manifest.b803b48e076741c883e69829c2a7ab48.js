10f30ab176e75b51d08c1ad01bb1863c57f39b8d
michyen <michyen@gmail.com>
2019-04-20 19:46:25 -0700

    deploy

---
+++ precache-manifest.b803b48e076741c883e69829c2a7ab48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "715ce2443f60262cfb03",
+    "url": "/static/css/main.74f445ad.chunk.css"
+  },
+  {
+    "revision": "715ce2443f60262cfb03",
+    "url": "/static/js/main.9475d21d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "839d7d411be935630af7",
+    "url": "/static/js/2.018939a5.chunk.js"
+  },
+  {
+    "revision": "0511472b45a2cd285f74e9968d89e28e",
+    "url": "/static/media/black.0511472b.png"
+  },
+  {
+    "revision": "a91ced508c1f29c87157adc980402a62",
+    "url": "/static/media/white.a91ced50.png"
+  },
+  {
+    "revision": "b9902d0b78770c453fe0e93ed22609e5",
+    "url": "/index.html"
+  }
+];
